e8250880d3af96f6cc7afa8b499c99c125106730
prasath.v <prasath.v@siamcomputing.com>
2024-04-20 22:23:07 +0530

    updated code

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,5 @@
 export const PORT = 5555;
+
+// 17GMail-joe-csevprasath17--rootbookstore
+
+export const mongoDBUrl = 'mongodb+srv://csevprasath17:rootbookstore@bookmern.07jpdo5.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookmern';
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
 import express from 'express';
-import { PORT } from './config.js';
+import mongoose from 'mongoose';
+import { PORT, mongoDBUrl } from './config.js';
 
 const app = express();
+
+mongoose
+    .connect(mongoDBUrl)
+    .then( () => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log('Server listening on port:', PORT);
+        });
+    })
+    .catch( error => {
+        console.log(error);
+    })
 
 // GET 
 app.get('/', (req, res) => {
     console.log('req => ', req);
     return res.status(235).send('Welcome to MERN Stack');
-});
-
-app.listen(PORT, () => {
-    console.log('Server listening on port:', PORT);
 });
